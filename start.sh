b655970a177848f044fee87f7ae148e3ae463dc2
Jasper Zhao <wmyjzzjh@gmail.com>
2021-08-15 15:26:19 +0800

    优化启停脚本

--- start.sh
+++ start.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-nohup npm start >> /home/ffmpeg/server.log 2>&1 &
+dir="/home/ffmpeg"
+logfile="server.log"
+#logfile="server-dev.log"
+if [ ! -d "$dir"]; then
+  mkdir -p $dir
+fi
+
+if [ ! -f "$dir/$logfile"]; then
+  touch $dir/$logfile
+  #chown 1000:1000 $dir/$logfile
+fi
+
+nohup npm start >> $dir/$logfile 2>&1 &
+#nohup npm run dev >> $dir/$logfile 2>&1 &
--- stop.sh
+++ stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-mypid=`ps -ef | grep node | awk 'NR==2{print $2}'`
-kill -9 $mypid
+mainjs="app.js"
+#mainjs="app_multi_thread.js"
+nodepid=`ps -ef | grep "node $mainjs" | awk 'NR==1{print $2}'`
+kill -9 $nodepid
