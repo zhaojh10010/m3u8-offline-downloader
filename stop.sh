fda98e8291f3a95c3f9c729b6715680cfa09e771
Jasper Zhao <wmyjzzjh@gmail.com>
2021-08-16 22:58:23 +0800

    测试

--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mainjs="app.js"
 #mainjs="app_multi_thread.js"
-nodepid=`ps -ef | grep "node $mainjs" | awk 'NR==1{print $2}'`
+nodepid=`ps -ef | grep "node $mainjs" | awk 'NR==3{print $2}'`
 kill -9 $nodepid
